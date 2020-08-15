d284fdad19ea2232a2f73cb6a0750ae4fd264f41
alexinto <69119689+alexinto@users.noreply.github.com>
2020-08-15 09:27:53 -0300

    Add files via upload
    123

--- bt.js
+++ bt.js
@@ -208,22 +208,18 @@ let readBuffer = '';
 
 // Получение данных
 function handleCharacteristicValueChanged(event) {
-  let value = new TextDecoder().decode(event.target.value);
-  receive(value);
-
-  for (let c of value) {
-    if (c === '\n') {
-      let data = readBuffer.trim();
-      readBuffer = '';
-
-      if (data) {
-        receive(data);
-      }
-    }
-    else {
-      readBuffer += c;
-    }
+  let value = event.target.value;
+  let a = [];
+  let perc = [];
+  // Convert raw data bytes to hex values just for the sake of showing something.
+  // In the "real" world, you'd use data.getUint8, data.getUint16 or even
+  // TextDecoder to process raw data bytes.
+  for (let i = 0; i < value.byteLength; i++) {
+    a.push('0x' + ('00' + value.getUint8(i).toString(16)).slice(-2));
   }
+  receive(a);
+  perc.push('Percent: ' + ('00' + value.getUint8(7).toString(10)).slice(-2) + '%'); 
+  receive(perc);
 }
 
 // Обработка полученных данных
